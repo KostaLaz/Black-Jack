75ea893a502b1480290725cf364154e21b8a45b2
KostaLaz <44593214+KostaLaz@users.noreply.github.com>
2019-12-08 18:59:05 +0100

    Adding images and sounds

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,17 @@
+let blackJackGame = {
+    'you': {'scoreSpan': '#your-black-jack-result', 'div': '#your-box', 'score': 0},
+    'dealer': {'scoreSpan': '#dealer-black-jack-result', 'div': '#dealer-box', 'score': 0} 
+
+};
+const YOU = blackJackGame['you'];
+const DEALER = blackJackGame['dealer'];
+const HIT_SOUND = new Audio('static/sounds/swish.m4a');
+
 document.querySelector('#hit-btn').addEventListener('click', blackJackHit);
 function blackJackHit(){
-    alert('HIT!');
+    let cardImg = document.createElement('img');
+    cardImg.src = 'static/images/Q.png'
+    document.querySelector(YOU['div']).appendChild(cardImg);
+    HIT_SOUND.play();
 }
+
